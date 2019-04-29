728967b9ad5c3ec644853031bae7a745b82b3487
Guillaume Isabelle <jgi@jgwill.com>
2019-04-29 16:30:38 -0400

    tstpath

--- gulping/gulpfile-elements.js
+++ gulping/gulpfile-elements.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 var { outputPath, projectName } = require('gixng');
 
 var target_dist = process.env.TARGET_DIST;//'/w/_vh/guillaumeisabelle.com/xng/';
+console.log(target_dist);
+
 
 var source_dist_path ='./' + outputPath + '/**';
 
---
+++ gulping/tst-path.js
@@ -0,0 +1,7 @@
+
+require('dotenv').config();
+
+var { outputPath, projectName } = require('gixng');
+
+var target_dist = process.env.TARGET_DIST;//'/w/_vh/guillaumeisabelle.com/xng/';
+console.log(target_dist);
