97daaa6f599960e3a33909230d6efdbb43878e7b
Guillaume Isabelle <jgi@jgwill.com>
2019-04-30 09:43:37 -0400

    build: gulp dist

--- elements/gulpfile.js
+++ elements/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 
 var clean = require('gulp-clean');
+var path = require('path');
 
 require('dotenv').config();
 
@@ -30,6 +31,13 @@ var source_dist_path_assets ='./' + outputPath + '/assets/**';
 
 gulp.task('copy-dist:assets', function() {
   return gulp.src(source_dist_path_assets)
+    .pipe(gulp.dest(target_dist + 'assets/'));
+});
+
+var source_dist_path_favicon ='./' + outputPath + '/favicon.ico';
+
+gulp.task('copy-dist:favicon', function() {
+  return gulp.src(source_dist_path_favicon)
     .pipe(gulp.dest(target_dist));
 });
 
@@ -37,6 +45,12 @@ gulp.task('copy-elements', function() {
   return gulp.src('./elements/*')
     .pipe(gulp.dest(target_dist));
 });
+
+
+gulp.task(
+  "dist",
+  gulp.series("copy-elements", "copy-dist:favicon","copy-dist:assets","copy-dist:style")
+);
 
 //@issue can not clean outside...
 gulp.task('clean-dist', function () {
